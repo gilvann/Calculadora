1258d6715eb020ea26838e4509b3cbea0def6692
gilvann <gilvann.lourenco@gmail.com>
2021-04-12 18:05:25 -0300

    Tratamento dos Números

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -32,9 +32,39 @@ class CalcController{
     clearEntry(){
         this._operation.pop();
     }
+    
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value){
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
+    }
+
     //Método PUSH adiciona o elemento na proxima posição do array
     addOperation(value){
-        this._operation.push(value);
+        if(isNaN(this.getLastOperation())){
+            //String
+            if(this.isOperator(value)){
+                this.setLastOperation(value);
+
+            }else if (isNaN(value)){
+                console.log(value);
+
+            }else{
+                this._operation.push(value);
+
+            }
+        }else{
+            //Number
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue))
+        }
+        console.log(this._operation);
     }
 
     setError(){
@@ -50,22 +80,37 @@ class CalcController{
                 this.clearEntry();
                 break;
             case 'soma':
-                this.teste();
+                this.addOperation('+');
                 break;
             case 'subtracao':
-
+                this.addOperation('-');
                 break;
             case 'divisao':
-
+                this.addOperation('/');
                 break;
             case 'multiplicacao':
-                this.test();
+                this.addOperation('*');
                 break;
             case 'porcento':
-
+                this.addOperation('%');
                 break;
             case 'igual':
-
+                
+                break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
                 break;
             default:
                 this.setError();
